bab4f4362bbacb8de60eb59d3c0e71c9aba7ac29
jasonmueller <jmuel9999@gmail.com>
2021-07-17 19:45:55 -0500

    finished idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,3 +1,20 @@
 let db;
 
 const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_deposit', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    
+    if (navigator.onLine) {
+        //uploadDeposit();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
