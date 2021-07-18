033a6d802dfe74ea1cbaf06b94c06d2415cbe199
jasonmueller <jmuel9999@gmail.com>
2021-07-18 08:02:23 -0500

    Added file manifest.json

--- public/js/idb.js
+++ public/js/idb.js
@@ -11,10 +11,17 @@ request.onsuccess = function(event) {
     db = event.target.result;
     
     if (navigator.onLine) {
-        //uploadDeposit();
+        uploadDeposit();
     }
 };
 
 request.onerror = function(event) {
     console.log(event.target.errorCode);
+};
+
+// this function will be used if the fetch function's catch methods are executed
+function saveDeposit(deposit) {
+    const transaction = db.transaction(['new_deposit'], 'readwrite');
+    const depositObjectStore = transaction.objectStore('new_deposit');
+    depositObjectStore.add(deposit);
 };
